b0fc874a1fdc0016349c2ce554f1c3a319c291ac
thamudi <tamim.hamoudi@gmail.com>
2020-12-16 10:30:00 +0200

    class 06

---
+++ class-06/demo/scripts.js
@@ -0,0 +1,67 @@
+
+// alert("Hello World"); 
+
+// define a variable 
+var number = 10; // variable type number
+var pepsiDrink = "12 Pepsi"; // variable type string
+var correct = true; // variable type boolean that is true 
+var notCorrect = false; // variable type boolean that is false
+
+// alert(number);
+// alert(pepsiDrink);
+// alert(correct);
+// alert(notCorrect);
+
+// if number is larger than 10 
+// if 15 is larger than 10
+// if 5 is larger than 10 
+
+// First condition to check if the number is larger than something 
+// if (number > 10 ) {
+//    alert(number);
+// } else {
+//     alert("The number is less that 10");
+// }
+
+
+//  < less than
+// > larger than
+// == equal to
+// <= less than equal 
+// >= larger than equal 
+// != not equal to 
+
+// checking if the number is less than 10
+// if 5 is less than 10 
+// if (number < 10 ){
+//     alert(number);
+// } else {
+//     alert("the number is larger than 10");
+// }
+
+
+// if pepsiDrink is the same as 12 Pepsi
+
+// if (pepsiDrink == "12 Pepsi" ){
+//     alert("Yes pepsiDrink is 12 Pepsi");
+// } else {
+//     alert("No, it is not.")
+// }
+
+// if(notCorrect) {
+//     alert("correct")
+// }
+
+// If the number is equal or larger than 10
+// if(number => 10){
+//     alert(number)
+// }
+// If the number is less than or equal to 5
+// if(number <= 5){
+//     alert(number)
+// }
+
+// if number is not equal to 20 
+// if(number != 20) {
+//     alert(number)
+// }
